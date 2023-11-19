9b7e1df51b3c9adbc86c1eaf640e5d476ce44f05
annakarolmarinho <anna.silva@sptech.school>
2023-11-19 11:04:14 -0300

    adicionando tela de energia Limpa

---
+++ Algoritmo_/Site/public/quiz.js
@@ -0,0 +1,94 @@
+
+
+var questions = [
+    {
+        question:"Quem detém o recorde de mais títulos mundiais na Fórmula 1?",
+        answers: [
+          { option: "Ayrton Senna", correct: false },
+          { option: "Sebastan Vettel ", correct: false },
+          { option: "Lewis Hamilton", correct: true },
+          { option: "Michael Schumacher", correct: false },
+        ],
+      },
+      {
+        question: "Qual equipe de Fórmula 1 é conhecida como 'Scuderia'?",
+        answers: [
+            { option: "Ferrari", correct: true },
+            { option: "Red Bull Racing ", correct: false },
+            { option: "Mercedes", correct: false},
+            { option: "Mclaren", correct: false },
+        ],
+      },
+      {
+        question: "Em que ano a primeira corrida de Fórmula 1 aconteceu?",
+        answers: [
+            { option: "1946", correct: false },
+            { option: "1962 ", correct: false },
+            { option: "1950", correct: true},
+            { option: "1975", correct: false },
+        ],
+      },
+      {
+        question: "Quantas vezes Ayrton Senna foi campeão mundial de Fórmula 1?",
+        answers: [
+            { option: "1", correct: false },
+            { option: "2", correct: false },
+            { option: "3", correct: true},
+            { option: "4", correct: false },
+        ],
+      },
+      {
+        question: "Qual circuito é conhecido como 'Templo da Velocidade'?",
+        answers: [
+            { option: "Circuit de Monaco", correct: false },
+            { option: "Silverstone Circuit ", correct: false },
+            { option: " Spa-Francorchamps", correct: false},
+            { option: "Autódromo Nazionale Monza ", correct: true },
+        ],
+      },
+      {
+        question: "Qual piloto é conhecido como 'O Profeta'?",
+        answers: [
+            { option: "Alain Prost", correct: true },
+            { option: "Niki Lauda", correct: false },
+            { option: "Emerson Fittipaldi", correct: false},
+            { option: "Juan Manuel Fangio", correct: false },
+        ],
+      },
+      {
+        question: "Quantas equipes competem na Fórmula 1 atualmente?",
+        answers: [
+            { option: "8", correct: false },
+            { option: "9", correct: false },
+            { option: "10", correct: true},
+            { option: "12", correct: false},
+        ],
+      },
+      {
+        question: "Qual é a pista mais longa do calendário da Fórmula 1?",
+        answers: [
+          { option: " Circuit de Spa-Francorchamps", correct: true },
+          { option: "Circuit of the Americas", correct: false },
+          { option: "Baku City Circuit", correct: false },
+          { option: "Yas Marina Circuit", correct: false },
+        ],
+      },
+      {
+        question: "Quem é o piloto mais jovem a vencer uma corrida de Fórmula 1?",
+        answers: [
+            { option: "Sebastian Vettel", correct: false },
+            { option: "Max Verstappen", correct: true },
+            { option: "Lewis Hamilton", correct: false},
+            { option: "Fernando Alonso", correct: false },
+        ],
+      },
+      {
+        question: "Quantos pontos são concedidos ao piloto que faz a volta mais rápida na corrida?",
+        answers: [
+          { option: "1 ponto", correct: true },
+          { option: "2 pontos", correct: false },
+          { option: "3 pontos", correct: false },
+          { option: "4 pontos", correct: false },
+        ],
+      },
+    ];
